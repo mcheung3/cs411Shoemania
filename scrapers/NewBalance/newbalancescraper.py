2e867cbe5c7e717158681b3c6ca6981b63187a47
David Shan <dshan3@illinois.edu>
2018-03-24 22:41:53 -0700

    separated by categories

--- scrapers/NewBalance/newbalancescraper.py
+++ scrapers/NewBalance/newbalancescraper.py
@@ -5,20 +5,30 @@ import csv
 
 def main():
 	# url
-	shoe_page = 'http://www.newbalance.com/men-1/?prefn1=productClass&prefv1=Shoes'
+	#'http://www.newbalance.com/men-1/?prefn1=productClass&prefv1=Shoes'
+	shoe_cats = {
+		'running': 'http://www.newbalance.com/men/shoes/all-shoes/running/?prefn1=productClass&prefv1=Shoes',
+		'lifestyle': 'http://www.newbalance.com/men/shoes/all-shoes/lifestyle/?prefn1=productClass&prefv1=Shoes',
+		'training': 'http://www.newbalance.com/men/shoes/all-shoes/training/?prefn1=productClass&prefv1=Shoes',
+		'walking': 'http://www.newbalance.com/men/shoes/all-shoes/walking/?prefn1=productClass&prefv1=Shoes'
+	}
 
-	page = requests.get(shoe_page)
-	soup = BeautifulSoup(page.content, 'html.parser')
+	for cat, shoe_page in shoe_cats.items():
+		page = requests.get(shoe_page)
+		soup = BeautifulSoup(page.content, 'html.parser')
+		filename = 'newbalance_' + cat + '.csv'
 
-	for shoe in soup.find_all('li', class_='tile'):
-		shoe_info = get_shoe_data(shoe)
-		# open a csv file with append, so old data will not be erased
-		with open('newbalance.csv', 'a') as csv_file:
- 			writer = csv.writer(csv_file)
- 			writer.writerow([shoe_info[i] for i in range(len(shoe_info))])
+		for shoe in soup.find_all('li', class_='tile'):
+			shoe_info = get_shoe_data(shoe, cat)
+			if not shoe_info:
+				continue
+			# open a csv file with append, so old data will not be erased
+			with open(filename, 'a') as csv_file:
+ 				writer = csv.writer(csv_file)
+ 				writer.writerow([shoe_info[i] for i in range(len(shoe_info))])
 
 
-def get_shoe_data(shoe):
+def get_shoe_data(shoe, cat):
 	brand = 'New Balance'
 	colors = get_colors(shoe)
 	name = get_name(shoe)
@@ -26,12 +36,14 @@ def get_shoe_data(shoe):
 
 	link = shoe.find('div', class_='product')
 	link = link.get('data-quickview-url')
+	if not link:
+		return
 	page = requests.get(link)
 	soup = BeautifulSoup(page.content, 'html.parser')
 
 	description = get_product_description(soup)
 	photo = get_photo_url(soup)
-	return brand, photo, description, colors, name, price
+	return brand, cat, photo, description, colors, name, price
 
 def get_colors(shoe):
 	colors = shoe['data-product-hit']

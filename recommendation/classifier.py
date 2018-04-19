0898f1b44d7e892fc9ba91eb7ac435d00541900c
David Shan <dshan3@illinois.edu>
2018-04-19 15:48:14 -0500

    got images

--- recommendation/classifier.py
+++ recommendation/classifier.py
@@ -2,64 +2,77 @@ import matplotlib.pyplot as plt
 import numpy as np
 import MySQLdb
 import requests
+import urllib
+import cv2
+import sys
 
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
 
+user = sys.argv[1]
 db = MySQLdb.connect(host='shoemaniadbinstance.chnenegb17td.us-east-2.rds.amazonaws.com', user='mcheung3',passwd='shoemania', db='shoemania')
 c = db.cursor()
 
 # get all shoes rated by users
 c.execute("""
-	SELECT shoes. FROM shoes, users, ratedlists 
-	WHERE users.name = 'mcheung3' AND users.id = ratedlists.userid AND shoes.id = ratedlists.shoe_id
-	""")
+	SELECT shoes.*, ratedlists.liked FROM shoes, users, ratedlists 
+	WHERE users.name = %s AND users.id = ratedlists.user_id AND shoes.id = ratedlists.shoe_id
+	""", (user,))
 #c.execute("""SELECT photo FROM shoes""", (shoe_info))
+
 pred = c.fetchall()
 db.commit()
 db.close()
 
+labels = np.array([i[-1] for i in pred])
+shoe_data = np.array([i[:-1] for i in pred])
 
-# The digits dataset
-digits = datasets.load_digits()
+img_url = shoe_data[0][2]
+url_response = urllib.urlopen(img_url)
+img_array = np.array(bytearray(url_response.read()), dtype=np.uint8)
+img = cv2.imdecode(img_array, -1)
 
-# The data that we are interested in is made of 8x8 images of digits, let's
-# have a look at the first 4 images, stored in the `images` attribute of the
-# dataset.  If we were working from image files, we could load them using
-# matplotlib.pyplot.imread.  Note that each image must have the same size. For these
-# images, we know which digit they represent: it is given in the 'target' of
-# the dataset.
-images_and_labels = list(zip(digits.images, digits.target))
-for index, (image, label) in enumerate(images_and_labels[:4]):
-    plt.subplot(2, 4, index + 1)
-    plt.axis('off')
-    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-    plt.title('Training: %i' % label)
 
-# To apply a classifier on this data, we need to flatten the image, to
-# turn the data in a (samples, feature) matrix:
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
+# # The digits dataset
+# digits = datasets.load_digits()
 
-# Create a classifier: a support vector classifier
-classifier = svm.SVC(gamma=0.001)
+# # The data that we are interested in is made of 8x8 images of digits, let's
+# # have a look at the first 4 images, stored in the `images` attribute of the
+# # dataset.  If we were working from image files, we could load them using
+# # matplotlib.pyplot.imread.  Note that each image must have the same size. For these
+# # images, we know which digit they represent: it is given in the 'target' of
+# # the dataset.
+# images_and_labels = list(zip(digits.images, digits.target))
+# for index, (image, label) in enumerate(images_and_labels[:4]):
+#     plt.subplot(2, 4, index + 1)
+#     plt.axis('off')
+#     plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+#     plt.title('Training: %i' % label)
 
-# We learn the digits on the first half of the digits
-classifier.fit(data[:n_samples // 2], digits.target[:n_samples // 2])
+# # To apply a classifier on this data, we need to flatten the image, to
+# # turn the data in a (samples, feature) matrix:
+# n_samples = len(digits.images)
+# data = digits.images.reshape((n_samples, -1))
 
-# Now predict the value of the digit on the second half:
-expected = digits.target[n_samples // 2:]
-predicted = classifier.predict(data[n_samples // 2:])
+# # Create a classifier: a support vector classifier
+# classifier = svm.SVC(gamma=0.001)
 
-print("Classification report for classifier %s:\n%s\n"
-      % (classifier, metrics.classification_report(expected, predicted)))
-print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
+# # We learn the digits on the first half of the digits
+# classifier.fit(data[:n_samples // 2], digits.target[:n_samples // 2])
 
-images_and_predictions = list(zip(digits.images[n_samples // 2:], predicted))
-for index, (image, prediction) in enumerate(images_and_predictions[:4]):
-    plt.subplot(2, 4, index + 5)
-    plt.axis('off')
-    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-    plt.title('Prediction: %i' % prediction)
+# # Now predict the value of the digit on the second half:
+# expected = digits.target[n_samples // 2:]
+# predicted = classifier.predict(data[n_samples // 2:])
 
-plt.show()
+# print("Classification report for classifier %s:\n%s\n"
+#       % (classifier, metrics.classification_report(expected, predicted)))
+# print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
+
+# images_and_predictions = list(zip(digits.images[n_samples // 2:], predicted))
+# for index, (image, prediction) in enumerate(images_and_predictions[:4]):
+#     plt.subplot(2, 4, index + 5)
+#     plt.axis('off')
+#     plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+#     plt.title('Prediction: %i' % prediction)
+
+# plt.show()

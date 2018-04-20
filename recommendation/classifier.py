3435346d1fe2025d519f0896cad906d1af18a517
David Shan <dshan3@illinois.edu>
2018-04-20 00:24:09 -0500

    im retarded

--- recommendation/classifier.py
+++ recommendation/classifier.py
@@ -28,8 +28,8 @@ pred = c.fetchall()
 
 # get all shoes not rated by users
 c.execute("""
-	SELECT shoes.* FROM shoes, users, ratedlists 
-	WHERE users.id = %s AND users.id NOT IN (SELECT ratedlists.shoe_id FROM ratedlists WHERE users.id = ratedlists.user_id)
+	SELECT shoes.* FROM shoes 
+	WHERE shoes.id NOT IN (SELECT ratedlists.shoe_id FROM ratedlists WHERE ratedlists.user_id = %s)
 	""", (user,))
 notrated = c.fetchall()
 
@@ -38,7 +38,7 @@ db.close()
 
 labels = np.array([i[-1] for i in pred])
 shoe_data = np.array([i[:-1] for i in pred])
-test_data = np.array(pred)
+shoe_data_test = np.array(notrated)
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 ## Functions
@@ -74,15 +74,16 @@ def create_json(shoe):
 
 #start = time.time()
 images = get_img_as_arr(shoe_data)
-test_images = get_img_as_arr(test_data)
+test_images = get_img_as_arr(shoe_data_test)
 #end = time.time()
 #print(end - start)
 
 n_samples = len(images)
+n_test = len(test_images)
 data = images.reshape((n_samples, -1))
-test_data = test_images.reshape((n_samples, -1))
+test_data = test_images.reshape((n_test, -1))
 
-classifier = svm.SVC(gamma=0.001)
+classifier = svm.SVC()
 
 classifier.fit(data, labels)
 

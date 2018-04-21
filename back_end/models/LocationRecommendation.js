8189d912968b0e729bb58ac0aecf3dc2b384bc50
Matthew Cheung <mcheung3@illinois.edu>
2018-04-21 18:28:05 -0500

    asdf

--- back_end/models/LocationRecommendation.js
+++ back_end/models/LocationRecommendation.js
@@ -11,5 +11,21 @@ var LocationRecommendation = {
                     });
      	});
     }
+    /**
+	getLocationRecommendation: function(username, callback) {  
+			return db.query("SELECT id FROM users WHERE users.name = ?", [username],  function(err, rows) { 
+			console.log("made it");
+			user_id = rows[0].id;
+			console.log(user_id)
+			var PythonShell = require('python-shell');
+			var options = {
+				scriptPath: '../recommendation/',
+				args: [user_id]
+			};
+			PythonShell.run('classifier.py', options, callback);
+		});
+    }
+
+    **/
 };  
 module.exports = LocationRecommendation; 
